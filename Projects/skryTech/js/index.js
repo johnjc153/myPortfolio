b210b365a72d0b1804f4135841d4efa7194d0cdb
johnjc153 <153johnjc@gmail.com>
2021-08-07 03:03:57 -0700

    made accordion close on open of different section, two buttons, close all, open all

--- Projects/skryTech/js/index.js
+++ Projects/skryTech/js/index.js
@@ -1,14 +1,50 @@
 const toggle = document.querySelector('.toggle');
 const navigation = document.querySelector('.navigation');
-const accordion = document.getElementsByClassName('contentBx');
+
 
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('active')
     navigation.classList.toggle('active')
 })
 
-for( i = 0; i < accordion.length; i++){
-    accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
-    })
-}
+let acc = document.getElementsByClassName("contentBx");
+        let i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].onclick = function () {
+
+                if (!this.classList.contains('active')) {
+                    closeAll();
+                }
+
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            }
+        }
+
+        function openAll() {
+            for (i = 0; i < acc.length; i++) {
+                acc[i].classList.add("active");
+            }
+        }
+
+        function closeAll() {
+            for (i = 0; i < acc.length; i++) {
+                acc[i].classList.remove("active");
+                acc[i].nextElementSibling.style.maxHeight = null;
+            }
+        }
+
+        document.getElementById('openAll').addEventListener('click', openAll);
+        document.getElementById('closeAll').addEventListener('click', closeAll);
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener('click', function () {
+                this.classList.toggle('active')
+            })
+        }
