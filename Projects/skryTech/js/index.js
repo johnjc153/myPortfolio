043b288aec43ff98032eb14049bd2ef5c2cb2c8b
johnjc153 <153johnjc@gmail.com>
2021-08-18 17:42:47 -0700

    formatting, added id

--- Projects/skryTech/js/index.js
+++ Projects/skryTech/js/index.js
@@ -7,58 +7,61 @@ toggle.addEventListener('click', () => {
 })
 
 let acc = document.getElementsByClassName("contentBx");
-        let i;
+let i;
 
 
-        for (i = 0; i < acc.length; i++) {
-            acc[i].onclick = function () {
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function () {
 
-                if (!this.classList.contains('active')) {
-                    closeAll();
-                }
-
-                this.classList.toggle("active");
-                var panel = this.nextElementSibling;
-                if (panel.style.maxHeight) {
-                    panel.style.maxHeight = null;
-                } else {
-                    panel.style.maxHeight = panel.scrollHeight + "px";
-                }
-            }
+        if (!this.classList.contains('active')) {
+            closeAll();
         }
 
-        function openAll() {
-            for (i = 0; i < acc.length; i++) {
-                acc[i].classList.add("active");
-            }
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
         }
+    }
+}
 
-        function closeAll() {
-            for (i = 0; i < acc.length; i++) {
-                acc[i].classList.remove("active");
-                acc[i].nextElementSibling.style.maxHeight = null;
-            }
-        }
+function openAll() {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].classList.add("active");
+    }
+}
+
+function closeAll() {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].classList.remove("active");
+        acc[i].nextElementSibling.style.maxHeight = null;
+    }
+}
 
 
-        // this works but doesnt close after another section is clicked
-        // for (i = 0; i < acc.length; i++){
-        //     acc[i].onclick = function () {
-        //         this.classList.toggle("active")
-        //     }
-        // }
-        
-        //works but does not close when section is clicked again
-        // for (i = 0; i < acc.length; i++) {
-        //     acc[i].addEventListener('click', function () {
-        //         this.classList.toggle('active')
-        //         })
-        // } 
+// this works but doesnt close after another section is clicked
+// for (i = 0; i < acc.length; i++){
+//     acc[i].onclick = function () {
+//         this.classList.toggle("active")
+//     }
+// }
 
-        
-        
+//works but does not close when section is clicked again
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener('click', function () {
+//         this.classList.toggle('active')
+//         })
+// } 
 
-        document.getElementById('openAll').addEventListener('click', openAll);
-        document.getElementById('closeAll').addEventListener('click', closeAll);
+function dropDown() {
+    for (i = 0; i < acc.length; i++) {
+        this.classList.toggle("active")
+    }
 
-       
+}
+
+document.getElementById('openAll').addEventListener('click', openAll);
+document.getElementById('closeAll').addEventListener('click', closeAll);
+document.getElementById('dropDown').addEventListener('click', dropDown);
