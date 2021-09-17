bc86878984d980827e763c2b53a4dd38c7ab42fd
johnjc153 <153johnjc@gmail.com>
2021-09-17 15:41:18 -0700

    added libraries, services section, section css

--- Projects/valleyRiver/assets/js/main.js
+++ Projects/valleyRiver/assets/js/main.js
@@ -1,7 +1,10 @@
+
 (function () {
   "use strict";
 
-  // selector function
+  /**
+   * Easy selector helper function
+   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -11,7 +14,9 @@
     }
   }
 
-  //lisentner function what
+  /**
+   * Easy event listener function
+   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -23,12 +28,16 @@
     }
   }
 
-  // scroll listener
+  /**
+   * Easy on scroll event listener 
+   */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  // navbar active link on scroll
+  /**
+   * Navbar links active state on scroll
+   */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -46,13 +55,15 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  // scroll to element w/ header offset
+  /**
+   * Scrolls to an element with header offset
+   */
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
 
     if (!header.classList.contains('header-scrolled')) {
-      offset -= 20
+      offset -= 16
     }
 
     let elementPos = select(el).offsetTop
@@ -62,21 +73,29 @@
     })
   }
 
-  // header scrolled class to header when page scrolled
+  /**
+   * Header fixed top on scroll
+   */
   let selectHeader = select('#header')
   if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
+    let headerOffset = selectHeader.offsetTop
+    let nextElement = selectHeader.nextElementSibling
+    const headerFixed = () => {
+      if ((headerOffset - window.scrollY) <= 0) {
+        selectHeader.classList.add('fixed-top')
+        nextElement.classList.add('scrolled-offset')
       } else {
-        selectHeader.classList.remove('header-scrolled')
+        selectHeader.classList.remove('fixed-top')
+        nextElement.classList.remove('scrolled-offset')
       }
     }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
+    window.addEventListener('load', headerFixed)
+    onscroll(document, headerFixed)
   }
 
-  // back to top btn
+  /**
+   * Back to top button
+   */
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -90,14 +109,18 @@
     onscroll(document, toggleBacktotop)
   }
 
-  // mobile nav toggle
+  /**
+   * Mobile nav toggle
+   */
   on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  //mobile dropdowns
+  /**
+   * Mobile nav dropdowns activate
+   */
   on('click', '.navbar .dropdown > a', function (e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
@@ -105,7 +128,9 @@
     }
   }, true)
 
-  // scroll ofset on links with .scrollto
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
   on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -121,8 +146,9 @@
     }
   }, true)
 
-
-  // scroll with ofset on page load with hash links in the url
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -131,12 +157,73 @@
     }
   });
 
-  // light box
-  const galleryLightbox = GLightbox({
-    selector: '.gallery-lightbox'
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function (e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function (el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function () {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
   });
 
-  // scroll animation
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
